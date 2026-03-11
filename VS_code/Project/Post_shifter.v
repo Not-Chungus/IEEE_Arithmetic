5a58a344cf07df9ac265a8d29d6dfffde26c3cc0
Ibrahim Mohamed PC <Ibrahimmmhanafy@hotmail.com>
2026-03-12 01:43:46 +0200

    solved -1 edge now for exp  final in control and pack

--- VS_code/Project/Post_shifter.v
+++ VS_code/Project/Post_shifter.v
@@ -12,7 +12,7 @@ module Post_shifter(
 //Counters (for left shifting)=================================================
     wire [4:0] count_zeros, count_ones; //5 bits
     wire [31:0] to_be_counted_zeros, to_be_counted_ones; //32 bits input for the LZC and LOC modules
-    wire allzeros_flag, allones_flag;
+    //wire allzeros_flag, allones_flag;
 
     assign to_be_counted_zeros = {S_sum[26:0], 5'b11111}; //5 ones padding, dont count MSB                           
     assign to_be_counted_ones = {S_sum[26:0], 5'b00000}; //5 zeros padding, dont count MSB                           
@@ -20,18 +20,18 @@ module Post_shifter(
     Leading_Zero_32bit LZC_32bit(
         .X(to_be_counted_zeros), //X is 32 bits
         .q(count_zeros),         //q is 5 bits
-        .a(allzeros_flag) // we dont need the a output for this module
+        .a(/*allzeros_flag*/) // we dont need the a output for this module
     );
 
     Leading_One_32bit LOC_32bit(
         .X(to_be_counted_ones), //X is 32 bits
         .q(count_ones),         //q is 5 bits
-        .a(allones_flag) // we dont need the a output for this module
+        .a(/*allones_flag*/) // we dont need the a output for this module
     );
 
 
     reg [27:0] S_normalized_intermediate;
-    //wire [27:0] mag = S_sum[27] ? ((~S_sum) + 28'd1) : S_sum;
+    
 
 
     //Two seperate shifters
@@ -56,16 +56,25 @@ module Post_shifter(
             if((S_sum[27] == S_sum[26]) && (S_sum[27:0] != 28'd0)) begin //too small [11 (small -ve), 00 (small +ve)]
                 
                 if(S_sum[27] == 1'b0) begin //detect zeros :LZC
-                S_normalized = S_sum << count_zeros;
+                S_normalized_intermediate = S_sum << count_zeros;
                 exp_adjustment = ~{3'b000,count_zeros} + 1; // decrease the exponent (-ve)
+                S_normalized = S_normalized_intermediate; 
 
-                end else begin //detect ones :LOC
-                S_normalized = S_sum << count_ones;
-                exp_adjustment = ~{3'b000,count_ones} + 1; // decrease the exponent (-ve)
+                end else begin //detect ones :LOC  [problem with -0.5, -0.25 that should output a -1 not -2]
+                S_normalized_intermediate = S_sum << count_ones;
+                //exp_adjustment = ~{3'b000,count_ones} + 1; // decrease the exponent (-ve)
+                    if (S_normalized_intermediate == 28'h8000000) begin //-2 (problem)
+                        S_normalized = 28'hc000000; //(-1)
+                        exp_adjustment = ~{3'b000,count_ones} + 2;
+                    end else begin
+                        S_normalized = S_normalized_intermediate;
+                        exp_adjustment = ~{3'b000,count_ones} + 1;
+                    end
                 end
 
             end else begin //output already normalized, do nothing
-                S_normalized = S_sum;
+                S_normalized_intermediate = S_sum;
+                S_normalized = S_normalized_intermediate;
                 exp_adjustment = 0;
             end
 
@@ -73,11 +82,12 @@ module Post_shifter(
     end
 
 
+
         
 
 
 endmodule
 
-//problems with boundry [-1] [Solving with mag]
+//problems with boundry [-1] [solving with brute force check]
 //problem with 0 result [DONE]?
 
